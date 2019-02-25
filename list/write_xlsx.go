7ccc8f7b5f93759de56e757e8a4991ed41ecc91f
Jared Howard <jared.howard@walmartlabs.com>
2019-02-25 12:41:22 -0700

    correcting output filename

--- list/write_xlsx.go
+++ list/write_xlsx.go
@@ -1,21 +1,51 @@
 package list
 
 import (
+	"os"
+	"os/user"
+	"path"
 	"strconv"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
-func WriteXLSX(xlsxFilename string, list *List) error {
-	if xlsxFilename == "" {
-		xlsxFilename = DefaultOutputAlphaPriceList
+func correctXLSXFilename(filename string) string {
+	if len(filename) == 0 {
+		filename = DefaultOutputAlphaPriceList
 	}
+
+	// If the OS shell did not expand the `~` from the command argument.
+	if filename[0] == '~' {
+		usr, _ := user.Current()
+		filename = usr.HomeDir + filename[1:]
+	}
+
+	fi, err := os.Lstat(filename)
+	// Expecting an error here if a new file will be created.
+	if err != nil {
+		// Ensure that the file is not a directory.
+		if os.IsPathSeparator(filename[len(filename)-1]) {
+			filename = path.Join(filename, DefaultOutputAlphaPriceList)
+		}
+	} else {
+		if fi.Mode().IsDir() {
+			filename = path.Join(filename, DefaultOutputAlphaPriceList)
+		}
+	}
+
+	return filename
+}
+
+func WriteXLSX(xlsxFilename string, list *List) error {
+	xlsxFilename = correctXLSXFilename(xlsxFilename)
 
 	xlsx := excelize.NewFile()
 
+	// Sheet information
 	xlsx.MergeCell("Sheet1", "A1", "L1")
 	xlsx.SetCellValue("Sheet1", "A1", "Effective Date: "+list.EffectiveDate)
 
+	// Column header
 	xlsx.SetCellValue("Sheet1", "A2", "CS Code")
 	xlsx.SetCellValue("Sheet1", "B2", "Status")
 	xlsx.SetCellValue("Sheet1", "C2", "Product Name")
@@ -46,11 +76,13 @@ func WriteXLSX(xlsxFilename string, list *List) error {
 		xlsx.SetCellValue("Sheet1", "L"+ii, p.Comment)
 	}
 
+	// Style formatting the cells
 	style, _ := xlsx.NewStyle(`{"number_format":1}`)
 	xlsx.SetCellStyle("Sheet1", "F3", "G"+ii, style)
 	style, _ = xlsx.NewStyle(`{"number_format":2}`)
 	xlsx.SetCellStyle("Sheet1", "H3", "K"+ii, style)
 
+	// Set column widths
 	xlsx.SetColWidth("Sheet1", "A", "A", 9)
 	xlsx.SetColWidth("Sheet1", "B", "B", 7.5)
 	xlsx.SetColWidth("Sheet1", "C", "C", 40)
@@ -64,7 +96,9 @@ func WriteXLSX(xlsxFilename string, list *List) error {
 	xlsx.SetColWidth("Sheet1", "K", "K", 12.5)
 	xlsx.SetColWidth("Sheet1", "K", "L", 10.5)
 
+	// Set up auto filter on the column header
 	xlsx.AutoFilter("Sheet1", "A2", "L2", "")
+	// Freeze the header to allow it to stay on the top
 	xlsx.SetPanes("Sheet1", `{"freeze":true,"split":false,"x_split":0,"y_split":2,"top_left_cell":"A3","active_pane":"bottomLeft","panes":[{"sqref":"A3","active_cell":"A3","pane":"bottomLeft"}]}`)
 
 	return xlsx.SaveAs(xlsxFilename)
